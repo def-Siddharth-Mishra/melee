62b04324cfe862463ce65f502d743737acc6a274
siddharthmishra <siddharthmishra918@gmmail.com>
2024-06-11 00:57:15 +0530

    Passing data to the target Screen or rather filtering the data and showing them, as of now title and its meals are there but meals data needs styling.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => const MealsScreen(
-          title: 'Some title',
-          meals: [],
+        builder: (context) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     ); // Navigator.push(context, route)
@@ -37,7 +44,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
