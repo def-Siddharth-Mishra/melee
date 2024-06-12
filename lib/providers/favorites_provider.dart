c76fc6eb92ad235fdc347960d70073f88ddaa9fa
siddharthmishra <siddharthmishra918@gmmail.com>
2024-06-12 16:02:28 +0530

    favorites_provider.dart file is created to favorite meals item after toggle.

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,23 @@
+import 'package:meals_app/models/meal.dart';
+import 'package:riverpod/riverpod.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleFavoriteMealsStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final FavoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+  (ref) {
+    return FavoriteMealsNotifier();
+  },
+);
